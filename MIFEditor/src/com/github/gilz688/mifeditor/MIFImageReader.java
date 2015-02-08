b7861997687fe80fd52689ab01242cc6adb581cc
Gilmore <eromlig.ac@gmail.com>
2015-02-08 14:11:50 +0800

    initial commit

---
+++ MIFEditor/src/com/github/gilz688/mifeditor/MIFImageReader.java
@@ -0,0 +1,62 @@
+package com.github.gilz688.mifeditor;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MIFImageReader {
+	public MIFImage read(File file) throws FileNotFoundException, IOException{
+		MIFImage image = new MIFImage();
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line;
+		Pattern initVar = Pattern.compile("\\s*(\\w+)\\s*=\\s*(\\w+);\\s*$");
+		while ((line = br.readLine()) != null) {
+			Matcher dMatcher = initVar.matcher(line);
+			if (dMatcher.matches()) {
+				String variable = dMatcher.group(1);
+				String value = dMatcher.group(2);
+				image.putVariable(variable, value);
+			} else if (line.trim().equalsIgnoreCase("BEGIN")) {
+				Pattern memRange = Pattern
+						.compile("\\s*\\x5b(\\d+)\\x2e{2}(\\d+)\\x5d\\s*:\\s+(.*);\\s*$");
+				Pattern memLoc = Pattern
+						.compile("\\s*(\\d+)\\s*:\\s+(.*);\\s*$");
+
+				while ((line = br.readLine()) != null) {
+					Matcher mrMatcher = memRange.matcher(line);
+					if (mrMatcher.matches()) {
+						int start = Integer.parseInt(mrMatcher.group(1));
+						int end = Integer.parseInt(mrMatcher.group(2));
+						int value = Integer.parseInt(mrMatcher.group(3), 2);
+						for (int i = start; i <= end; i++)
+							image.putData(i, value);
+					} else {
+						Matcher mlMatcher = memLoc.matcher(line);
+						if (mlMatcher.matches()) {
+							int address = Integer.parseInt(mlMatcher.group(1));
+							String[] values = mlMatcher.group(2).split(" ");
+							if (image.getpWidth() == -1)
+								image.setpWidth(values.length);
+							for (String str : values) {
+								int value = Integer.parseInt(str, 2);
+								image.putData(address, value);
+								address++;
+							}
+						}
+					}
+					if (line.trim().equalsIgnoreCase("End;")) {
+						if(image.size()==19200){
+							image.setpWidth(160);
+						}
+						break;
+					}
+				}
+			}
+		}
+		br.close();
+		return image;
+	}
+}
---
+++ MIFEditor/src/com/github/gilz688/mifeditor/MIFImageWriter.java
@@ -0,0 +1,43 @@
+package com.github.gilz688.mifeditor;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map.Entry;
+
+public class MIFImageWriter {
+	private MIFImage image;
+	
+	public MIFImageWriter(MIFImage image){
+		this.image = image;
+	}
+	
+	public void write(File file) throws IOException{
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		for(Entry<String,String> entry : image.getVariables().entrySet()){
+			bw.write(entry.getKey()+" = "+entry.getValue()+";\n");
+		}
+		bw.write("Content\nBEGIN\n");
+		bw.write("\t[0.." + String.valueOf(image.size()-1)+"] : 000000;\n");
+		int pWidth = image.getpWidth();
+		int rows = image.size()/pWidth;
+		for(int i=rows-1;i>-1;i--){
+			bw.write("\t"+i*pWidth+" : ");
+			for(int j=0;j<pWidth;j++){
+				String binStr = Integer.toBinaryString(image.getData(i*pWidth+j));
+				
+				for(int k=0;k<6-binStr.length();k++)
+					bw.write("0");
+				
+				bw.write(binStr);
+				if(j<pWidth-1)
+					bw.write(" ");
+				else
+					bw.write(";\n");
+			}
+		}
+		bw.write("End;\n");
+		bw.close();
+	}
+}
